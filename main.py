dfdd33f895079dd19e7cac8b9c1352ed2a3d0a94
emericwillard <emericwillard@gmail.com>
2021-02-13 22:31:05 +0100

    v4

--- main.py
+++ main.py
@@ -78,41 +78,55 @@ def create_obj():
 
 create_obj()
 
-while running:
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_s:
-                l_case = []
-                base_cas = True
-                screen.fill(black)
-                create_obj()
-        if base_cas:
-            if event.type == pg.MOUSEBUTTONUP:
-                x, y = event.pos
-                x = get_start(x)
-                y = get_start(y)
-                for c in range(len(l_case)):
-                    if x == l_case[c].x and y == l_case[c].y:
-                        l_case[c] = Make_rect(x, y)
-                        l_case[c].life = white
-                        pg.draw.rect(screen, l_case[c].life, l_case[c].set_rect())
+def main():
+
+    global running, base_cas, l_case
+
+    count = 0
+
+    while running:
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    base_cas = False
-    if not base_cas:
-        for c in l_case:
-            if c.life == black and c.get_next() == 3:
-                c.next = white
-            elif c.life == white and (c.get_next() < 2 or c.get_next() > 3):
-                c.next = black
-        screen.fill(black)
-        for c in range(len(l_case)):
-            pg.draw.rect(screen, l_case[c].life, l_case[c].set_rect())
-        for c in l_case:
-            if c.next is not None:
-                c.life = c.next
-    make_grid()
-    pg.display.update()
+                if event.key == pg.K_s:
+                    l_case = []
+                    base_cas = True
+                    screen.fill(black)
+                    create_obj()
+                elif event.key == pg.K_c:
+                    print(count)
+            if base_cas:
+                if event.type == pg.MOUSEBUTTONUP:
+                    x, y = event.pos
+                    x = get_start(x)
+                    y = get_start(y)
+                    for c in range(len(l_case)):
+                        if x == l_case[c].x and y == l_case[c].y:
+                            l_case[c] = Make_rect(x, y)
+                            l_case[c].life = white
+                            pg.draw.rect(screen, l_case[c].life, l_case[c].set_rect())
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_SPACE:
+                        base_cas = False
+        if not base_cas:
+            for c in l_case:
+                if c.life == black and c.get_next() == 3:
+                    c.next = white
+                elif c.life == white and (c.get_next() < 2 or c.get_next() > 3):
+                    c.next = black
+            screen.fill(black)
+            for c in range(len(l_case)):
+                pg.draw.rect(screen, l_case[c].life, l_case[c].set_rect())
+            for c in l_case:
+                if c.next is not None:
+                    c.life = c.next
+            count += 1
+        make_grid()
+        pg.display.update()
+
+
+if __name__ == '__main__':
+    main()
